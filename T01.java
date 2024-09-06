5c7c9227c08aee2581adde55e75683f79839eb1e
RospikaSiregar <rospikasarahsiregar@gmail.com>
2024-09-06 11:33:42 +0700

    Sudah menyelesaikan tugas T01

--- T01.java
+++ T01.java
@@ -1,1 +1,34 @@
-// NIM - Your Name
+//12S24008 - Rospika Sarah Yosefin Siregar
+//12S24013 - Adryan Julianto Panjaitan
+
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+        String judul;
+        String namaPenulis;
+        int tahunterbit;
+        String penerbit;
+        String formatbukupaperback;
+        double hargapembelian;
+        double minimummargin;
+        int stok;
+        double rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        namaPenulis = input.nextLine();
+        tahunterbit = input.nextInt();
+        penerbit = input.nextLine();
+        formatbukupaperback = input.nextLine();
+        hargapembelian = input.nextDouble();
+        minimummargin = input.nextDouble();
+        stok = input.nextInt();
+        rating = input.nextDouble();
+        System.out.println(iSBN + "|" + judul + "|" + namaPenulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukupaperback + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + rating);
+    }
+}
